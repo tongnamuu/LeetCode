80f2a85f20c4f807dbb6e7444fe7fbde69cad7cc
tongnamuu <thban33@naver.com>
2020-04-23 20:20:01 +0900

    Solution Code

---
+++ Bitwise AND of Numbers Range.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int rangeBitwiseAnd(int m, int n)
+    {
+        if (n == m)
+            return n;
+        while (m < n)
+        {
+            n -= (n & -n);
+        }
+        return n;
+    }
+};
