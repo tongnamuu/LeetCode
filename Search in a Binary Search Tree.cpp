6ec13e01713f843fb9c7a40c6da69dcfaef2de25
tongnamuu <thban33@naver.com>
2020-06-18 22:16:14 +0900

    June Daily Challenge Day15

---
+++ Search in a Binary Search Tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* node = root;
+        while (node) {
+            if (node->val == val)
+                return node;
+            else if (node->val > val)
+                node = node->left;
+            else if (node->val < val)
+                node = node->right;
+        }
+        return node;
+    }
+};
