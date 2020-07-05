605913da366e77978f3b73e064f2f631dca829f8
tongnamuu <thban33@naver.com>
2020-07-05 20:50:58 +0900

    July Daily Challenge Day04

---
+++ Ugly Number II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    int nthUglyNumber(int n) {
+        vector<int> ans;
+        for (long long i = 1; i < 2147483647; i *= 2) {
+            for (long long j = i; j < 2147483647; j *= 3) {
+                for (long long k = j; k < 2147483647; k *= 5) {
+                    ans.push_back(k);
+                }
+            }
+        }
+        cout << ans.size();
+        sort(ans.begin(), ans.end());
+        return ans[n - 1];
+    }
+};
