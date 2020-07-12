9fa400cd5faf3f29609a78317fbf246998fc0189
tongnamuu <thban33@naver.com>
2020-07-12 15:10:29 +0900

    May Challenge Solution Day 21

---
+++ Count Square Submatrices with All Ones.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        if(!n) return 0;
+        int m = matrix[0].size();
+        vector<vector<int>>d(n,vector<int>(m));// max_length of square, last, right point
+        int ans = 0;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(matrix[i][j]==1){
+                    int x = i?d[i-1][j]:0;
+                    int y = j?d[i][j-1]:0;
+                    int z = (i&&j)?d[i-1][j-1]:0;
+                    d[i][j]=min({x,y,z})+1;
+                    ans+=d[i][j];
+                }
+            }
+        }
+        return ans;
+    }
+};
