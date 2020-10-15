d6f5576ef5317f8904d4c963070654f4582b2c32
tongnamuu <thban33@naver.com>
2020-10-15 19:29:21 +0900

    October Daily Challenge Day08

---
+++ Binary Search.py
@@ -0,0 +1,6 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        idx = bisect.bisect_left(nums, target)
+        if idx < len(nums) and nums[idx] == target:
+            return idx
+        return -1
