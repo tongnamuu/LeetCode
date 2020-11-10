fffb5c2dbf92c2b0678c86cadfa29737e4c7f410
tongnamuu <thban33@naver.com>
2020-11-10 20:57:47 +0900

    November Daily Challenge Day 08

---
+++ Binary Tree Tilt.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    int ans = 0;
+    int solve(TreeNode* root) {
+        if (!root) return 0;
+        int left = solve(root->left);
+        int right = solve(root->right);
+        ans += abs(left - right);
+        return left + right + root->val;
+    }
+    int findTilt(TreeNode* root) {
+        solve(root);
+        return ans;
+    }
+};
