9a49970cf471017e9217b845461619d70beb6302
tongnamuu <thban33@naver.com>
2021-10-14 22:03:09 +0900

    Time: 280 ms (26.07%), Space: 9.1 MB (41.61%) - LeetHub

---
+++ perfect-squares/perfect-squares.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int>d(n+1);
+        for(int i=1;i<=n;++i){
+            d[i]=n;
+            for(int k=1;k*k<=i;++k){
+                d[i]=min(d[i-k*k]+1,d[i]);
+            }
+        }
+        return d[n];
+    }
+};
