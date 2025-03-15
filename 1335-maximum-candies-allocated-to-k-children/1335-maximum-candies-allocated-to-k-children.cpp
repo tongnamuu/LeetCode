fd3fc7303fd622964cbb9a958fa7c2aa52a07925
tongnamuu <thban33@naver.com>
2025-03-16 04:46:38 +0900

    Time: 51 ms (10.83%), Space: 88.1 MB (72.52%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+      int start = 0;
+      int end = 10000000;
+      int n = candies.size();
+      sort(candies.begin(), candies.end());
+      while(start<=end) {
+        int mid = (start + end) / 2;
+        if(ok(mid, candies, k)) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+      }
+      return end;
+
+    }
+    bool ok(int target, vector<int>& a, long long people) {
+        if(target == 0) return true;
+        for(int i : a) {
+            people -= i/target;
+        }
+        return people <= 0L;
+    }
+};
