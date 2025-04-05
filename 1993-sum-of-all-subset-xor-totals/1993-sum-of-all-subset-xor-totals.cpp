6e89d1661d3a9d0bc0a8822408269928b8d790b2
tongnamuu <thban33@naver.com>
2025-04-06 02:11:53 +0900

    Time: 3 ms (39.68%), Space: 9 MB (99.17%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i=1;i<(1<<n);i++) {
+            int res = 0;
+            for(int j=0;j<n;j++) {
+                if(i&(1<<j)) {
+                    res ^= nums[j];
+                }
+            }
+            ans += res;
+        }
+        return ans;
+    }
+};
