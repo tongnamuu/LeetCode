392e2f540e2db360f358aea26efba178246c4292
tongnamuu <thban33@naver.com>
2025-04-09 23:54:43 +0900

    Time: 7 ms (89.94%), Space: 31.2 MB (82.7%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-
         sort(nums.begin(), nums.end());
         nums.erase(unique(nums.begin(), nums.end()), nums.end());
         if(nums[0] < k) return -1;
