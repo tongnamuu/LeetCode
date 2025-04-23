bc4938660be991f2cbdc744709fd5df779569515
tongnamuu <thban33@naver.com>
2025-04-24 00:44:25 +0900

    Time: 1 ms (51.86%), Space: 8.3 MB (27.46%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int cnt[100000];
+    int countLargestGroup(int n) {
+        int ans = 0;
+        int val = 0;
+        for(int i=1;i<=n;i++) {
+            int d = digitSum(i);
+            cnt[d]++;
+            if(cnt[d]>ans) {
+                ans = cnt[d];
+                val = 1;
+            } else if(cnt[d]==ans) {
+                val++;
+            }
+        }
+        cout<<ans<<' '<<val<<'\n';
+        return val;
+    }
+
+    int digitSum(int x) {
+        int res = 0;
+        while(x) {
+            res += x % 10;
+            x/=10;
+        }
+        return res;
+    }
+};
