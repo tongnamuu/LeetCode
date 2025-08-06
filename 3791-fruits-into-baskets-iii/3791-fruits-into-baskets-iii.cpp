5be4fdacee5a00476db9bde9b97ce41243ab3a3b
tongnamuu <thban33@naver.com>
2025-08-07 01:43:21 +0900

    Time: 656 ms (11.41%), Space: 176.5 MB (97.21%) - LeetHub

---
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+        int m = 300;
+        int intervalCount = 340;
+        vector<int>interval(intervalCount);
+        for(int i=0;i<n;i++) {
+            interval[i/m] = max(interval[i/m], baskets[i]);
+        }
+        int ans = 0;
+        int i=0,j=0,k=0;
+        for(i=0;i<n;i++) {
+            int occupied = 0;
+            for(j=0;!occupied && j<intervalCount;j++) {
+                if(interval[j] < fruits[i]) continue;
+                interval[j] = 0;
+                for(k=0;k<m;k++) {
+                    int idx = j*m + k;
+                    if(idx>=n) break;
+                    if(idx< n && fruits[i] <= baskets[idx] && !occupied) {
+                        baskets[idx] = 0;
+                        occupied = 1;
+                    }
+                    if(idx< n) interval[j] = max(interval[j], baskets[idx]);
+                }
+                break;
+            }
+            if(!occupied) ans++;
+        }
+
+        return ans;
+    }
+};
