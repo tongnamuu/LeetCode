311c0427595c7c4c72dede72c4144b6b4e612987
tongnamuu <thban33@naver.com>
2025-10-02 14:15:38 +0900

    Time: 0 ms (100%), Space: 8.5 MB (77.56%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans = numBottles;
+        while(numBottles >= numExchange) {
+            numBottles -= numExchange;
+            numBottles += 1;
+            numExchange++;
+            ans++;
+        }
+        return ans;
+    }
+};
