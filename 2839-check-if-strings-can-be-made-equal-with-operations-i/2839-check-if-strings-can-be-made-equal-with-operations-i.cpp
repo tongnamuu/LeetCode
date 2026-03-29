e7c0dcc88e7bd60e7a758642cb3a71b53fb16c4f
tongnamuu <thban33@naver.com>
2026-03-29 22:03:14 +0900

    Time: 0 ms (100%), Space: 8.6 MB (32.9%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int cnt[26][2];
+    bool canBeEqual(string s1, string s2) {
+        int n = s1.length();
+        for(int i=0;i<n;i++) {
+            if(i&1) {
+                cnt[s1[i]-'a'][1]++;
+                cnt[s2[i]-'a'][1]--;
+            } else {
+                cnt[s1[i]-'a'][0]++;
+                cnt[s2[i]-'a'][0]--;
+            }
+        }
+        for(int i=0;i<26;i++) {
+            if(cnt[i][0]!=0 || cnt[i][1]!=0) return false;
+        }
+        return true;
+
+    }
+};
