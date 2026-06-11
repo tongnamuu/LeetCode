498ebe5246327e0f6cec220bf2b6659d3b1945f8
tongnamuu <thban33@naver.com>
2026-06-12 03:15:48 +0900

    Time: 585 ms (14.77%), Space: 279.7 MB (95.3%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> adj[100001];
+    int dfs(int now, int p) {
+        int temp = 0;
+        for(int next:adj[now]) {
+            temp = max(temp, dfs(next, now));
+        }
+        return temp + 1;
+    }
+    long long pow(int n) {
+        long long val = 1L;
+        long long p = 2L;
+        long long MOD = 1000000007L;
+        while(n) {
+            if(n&1) {
+                val *= p;
+                val %=MOD;
+            }
+            p *= p;
+            p%=MOD;
+            n /= 2;
+        }
+
+
+        return val;
+    }
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        for(auto& e : edges) {
+            adj[e[0]].push_back(e[1]);
+        }
+        int cnt = dfs(1, -1) - 1;
+        cout<<cnt<<'\n';
+        long long ans = pow(cnt - 1);
+        return ans;
+    }
+};
