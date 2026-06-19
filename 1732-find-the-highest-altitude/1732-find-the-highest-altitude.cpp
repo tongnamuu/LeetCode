56610246db3505cb0e4872ce7d989d630b33dbea
tongnamuu <thban33@naver.com>
2026-06-20 04:49:23 +0900

    Time: 0 ms (100%), Space: 10.9 MB (44.68%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
         int n = gain.size();
-        vector<int>x(n+1);
         int ans = 0;
+        int prev = 0;
         for(int i=0;i<n;i++) {
-            x[i+1] = x[i] + gain[i];
-            ans = max(ans, x[i+1]);
+            prev = prev + gain[i];
+            ans = max(ans, prev);
         }
         return ans;
     }
