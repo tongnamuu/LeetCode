7871f0fd93be56dc68749123ecf0d7e8b6ac6601
tongnamuu <thban33@naver.com>
2026-07-04 13:21:57 +0900

    Time: 8 ms (98.01%), Space: 105.9 MB (99.83%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int p[100001];
+    int r[100001];
+    int find(int x) {
+        if(x==p[x]) return x;
+        int nx = find(p[x]);
+        r[x] = min(r[x], r[nx]);
+        r[nx] = r[x];
+        p[x] = nx;
+        return p[x];
+    }
+    int merge(int x, int y, int c) {
+        x = find(x);
+        y = find(y);
+        if(x!=y) {
+            p[x] = y;
+        }
+        r[x] = min(c, min(r[x], r[y]));
+        r[y] = r[x];
+        return 1;
+    }
+    int minScore(int n, vector<vector<int>>& roads) {
+        for(int i=1;i<=n;i++) {
+            p[i] = i;
+            r[i] = 1000000000;
+        }
+        for(auto& road : roads) {
+            int x = road[0];
+            int y = road[1];
+            int c = road[2];
+            merge(x,y,c);
+            //cout<<x<<' '<<y<<' '<<c<<' '<<r[x]<<' '<<r[y]<<'\n';
+        }
+        for(int i=1;i<=n;i++) {
+            find(i);
+            // cout<<i<<' '<<p[i]<<' '<<r[i]<<'\n';
+        }
+        return r[n];
+    }
+};
