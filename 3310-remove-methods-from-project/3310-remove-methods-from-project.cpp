e9de57b5bea5782786b6f1e48a26e7eb652bd0a0
tongnamuu <thban33@naver.com>
2026-08-06 04:32:27 +0900

    Time: 144 ms (96.55%), Space: 304 MB (74.48%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<int> indegree(n);
+        vector<vector<int>>adj(n);
+        for(auto& e:invocations) {
+            int from = e[0];
+            int to = e[1];
+            adj[from].push_back(to);
+            indegree[to]++;
+        }
+        vector<int>ans;
+        queue<int>q;
+        vector<int>d(n, 0);
+        //if(indegree[k]==0) {
+            d[k] = 1;
+            q.push(k);
+        //}
+        while(!q.empty()) {
+            int now = q.front();
+            q.pop();
+            for(int next:adj[now]) {
+                indegree[next]--;
+                if(d[next]==0) {
+                    q.push(next);
+                    d[next] = 1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++) {
+            if(d[i]==0) ans.push_back(i);
+            else if(d[i]==1 && indegree[i]>0) {
+                vector<int>temp(n);
+                for(int j=0;j<n;j++) {
+                    temp[j] = j;
+                }
+                return temp;
+            }
+        }
+        return ans;
+    }
+};
