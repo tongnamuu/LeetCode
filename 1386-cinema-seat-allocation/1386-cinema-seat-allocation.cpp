b8d46983ddd1a35fd369b2f004e3b36da00dc911
tongnamuu <thban33@naver.com>
2026-08-19 23:39:45 +0900

    Time: 101 ms (17.23%), Space: 53.7 MB (99.32%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int deadCount(int* temp) {
+        if(temp[1]==1) {
+            if(temp[0]==1 && temp[2]==1) {
+                return 0;
+            }
+            return 1;
+        } else {
+            if(temp[0]==1 && temp[2]==1) {
+                return 0;
+            } else if(temp[0]==1 && temp[2]==0) {
+                return 1;
+            } else if(temp[0]==0 && temp[2]==1) {
+                return 1;
+            }
+            return 2;
+        }
+    }
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        sort(reservedSeats.begin(), reservedSeats.end(), [](const vector<int>& u, const vector<int>& v) {
+            if(u[0]==v[0]) {
+                return u[1] < v[1];
+            }
+            return u[0] < v[0];
+        });
+        int ans = n*2;
+        int cur = reservedSeats[0][0];
+        int i = 0;
+        int x = reservedSeats.size();
+        int temp[3] = {1,1,1};
+        while(i<x) {
+            if(cur==reservedSeats[i][0]) {
+                int reserve = reservedSeats[i][1];
+                //cout<<"making " << cur<<' '<<reserve<<'\n';
+                if(reserve==2||reserve==3) {
+                    temp[0] = 0;
+                } else if (reserve == 4|| reserve==5) {
+                    temp[0] = 0;
+                    temp[1] = 0;
+                } else if(reserve == 6 || reserve==7) {
+                    temp[1] = 0;
+                    temp[2] = 0;
+                } else if(reserve==8|| reserve == 9) {
+                    temp[2] = 0;
+                }
+                i++;
+            } else {
+                ans -= deadCount(temp);
+                // cout<<cur<<' '<<deadCount(temp)<<'\n';
+                // for(int i:temp) cout<<i<<' ';
+                // cout<<'\n';
+                cur = reservedSeats[i][0];
+                temp[0] = 1;
+                temp[1] = 1;
+                temp[2] = 1;
+            }
+        }
+        ans -= deadCount(temp);
+        //cout<<cur<<' '<<deadCount(temp)<<'\n';
+        return ans;
+    }
+};
